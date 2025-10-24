52c237163284911ffc0eafec033ef899cd890950
Silas Müller <dev@silasmueller.de>
2025-10-24 14:20:53 +0000

    added is_in_interval() method

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,10 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= 0 and ll_px <= self._dx \
-            and ll_py >= 0 and ll_py <= self._dy
+        return (
+            self.is_in_interval(ll_px, 0.0, self._dx) and
+            self.is_in_interval(ll_p, 0.0, self._dy)
+        )
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,8 +43,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
-
+    def is_in_interval(self, value: float, lower: float, upper: float) -> bool:
+        return lower <= value <= upper
 
 def test_rectangle_contains_exact() -> None:
     rectangle = Rectangle(lower_left=Point2D(1.0, 2.0), dx=2.5, dy=1.5)
